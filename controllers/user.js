9cf23dc3f9ed1ff29939776444b60572b3a0694e
honglc <hongmainmail@gmail.com>
2022-11-01 10:03:09 +0700

    fixed login error

--- controllers/user.js
+++ controllers/user.js
@@ -2,14 +2,12 @@ const User = require("../models/User");
 
 exports.login = (request, response, next) => {
   const user = request.body;
-  console.log("user:", user);
+  // console.log("user:", user);
   User.findOne({ email: user.email })
     .then((user) => {
-      console.log("user:", user);
+      // console.log("user:", user);
       if (user) {
         response.send({ isUserExist: true });
-      } else {
-        response.send(null);
       }
     })
     .catch((err) => console.log("::ERROR:", err));
