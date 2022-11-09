262378951749c11a786a6d64f0be33e8f10c0b05
honglc <hongmainmail@gmail.com>
2022-11-09 17:31:58 +0700

    found empty room from transaction by user booking date

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -5,19 +5,19 @@ const Room = require("../models/Room");
 
 // compare 2 date to know is later, earlier or same time
 const compareDate = (dateA, dateB) => {
-  if (dateA.getFullYear() > dateB.getFullYear()) {
+  if (dateA.getUTCFullYear() > dateB.getUTCFullYear()) {
     return "later";
-  } else if (dateA.getFullYear() < dateB.getFullYear()) {
+  } else if (dateA.getUTCFullYear() < dateB.getUTCFullYear()) {
     return "earlier";
   } else {
-    if (dateA.getMonth() > dateB.getMonth()) {
+    if (dateA.getUTCMonth() > dateB.getUTCMonth()) {
       return "later";
-    } else if (dateA.getMonth() < dateB.getMonth()) {
+    } else if (dateA.getUTCMonth() < dateB.getUTCMonth()) {
       return "earlier";
     } else {
-      if (dateA.getDate() > dateB.getDate()) {
+      if (dateA.getUTCDate() > dateB.getUTCDate()) {
         return "later";
-      } else if (dateA.getDate() < dateB.getDate()) {
+      } else if (dateA.getUTCDate() < dateB.getUTCDate()) {
         return "earlier";
       } else {
         return "same";
@@ -28,6 +28,7 @@ const compareDate = (dateA, dateB) => {
 
 // update transaction status until today
 const updateTransactionStatus = () => {
+  console.log("TRANSACTION UPDATED");
   const toDay = new Date();
   Transaction.find()
     .then((trans) => {
@@ -38,21 +39,24 @@ const updateTransactionStatus = () => {
         if (compateDateStart === "earlier" && compateDateEnd === "earlier") {
           Transaction.findByIdAndUpdate(tran._id, { status: "Booked" })
             .then((result) => {
-              // console.log("result:", result);
+              // console.log("Booked:", result);
             })
             .catch((err) => console.log("::ERROR:", err));
         }
         if (compateDateStart === "later" && compateDateEnd === "later") {
           Transaction.findByIdAndUpdate(tran._id, { status: "Checkout" })
             .then((result) => {
-              // console.log("result:", result);
+              // console.log("Checkout:", result);
             })
             .catch((err) => console.log("::ERROR:", err));
         }
-        if (compateDateStart === "same" && compateDateEnd === "earlier") {
+        if (
+          (compateDateStart === "same" && compateDateEnd === "earlier") ||
+          (compateDateStart === "later" && compateDateEnd === "earlier")
+        ) {
           Transaction.findByIdAndUpdate(tran._id, { status: "Checkin" })
             .then((result) => {
-              // console.log("result:", result);
+              // console.log("Checkin:", result);
             })
             .catch((err) => console.log("::ERROR:", err));
         }
@@ -216,22 +220,23 @@ const calTimeStay = (start, end) => {
   // convert date object to string
   const dateToString = (date) => {
     return (
-      date.getFullYear().toString() +
+      date.getUTCFullYear().toString() +
       "-" +
-      date.getMonth().toString() +
+      (date.getUTCMonth() + 1).toString() +
       "-" +
-      date.getDate().toString()
+      date.getUTCDate().toString()
     );
   };
 
   const stayCalendar = [dateToString(start)];
 
   // set number of day
-  const daysNumber = end.getDate() - start.getDate() - 1;
+  const daysNumber = end.getUTCDate() - start.getUTCDate() - 1;
   for (let i = 1; i <= daysNumber; i++) {
     let date = start;
     // add date of date object to 1
-    date.setDate(date.getDate() + 1);
+    date.setDate(date.getUTCDate() + 2);
+
     stayCalendar[i] = dateToString(date);
   }
 
@@ -249,11 +254,22 @@ exports.searchHotels = (request, response, next) => {
   const requestData = request.body;
   const person = requestData.options;
   const date = requestData.date;
-  // console.log("requestData:", requestData);
+  // console.log("===================================");
+  // console.log("requestData.date:", requestData.date);
 
   // convert date string to date object
+  // startDate = new Date(Date.UTC(date[0].startDate));
+  // endDate = new Date(Date.UTC(date[0].endDate));
   startDate = new Date(date[0].startDate);
   endDate = new Date(date[0].endDate);
+  // console.log(
+  //   "startDate:",
+  //   startDate.getUTCDate(),
+  //   "/",
+  //   startDate.getUTCMonth() + 1,
+  //   "/",
+  //   startDate.getUTCFullYear()
+  // );
 
   // delete all whitespace in string
   const location = requestData.destination.replace(/ /g, "").trim();
@@ -263,13 +279,15 @@ exports.searchHotels = (request, response, next) => {
 
   // calculate the calendar user will stay
   const stayCalendar = calTimeStay(startDate, endDate);
+  // console.log("stayCalendar:", stayCalendar);
 
   // search hotel by user request location
   searchHotelByLocation(location)
     .then((hotels) => {
       // console.log("hotels.length:", hotels.length);
       const emptyRooms = [];
-      let isEmptyRoom = false;
+      const emptyRoomFromTran = [];
+      let allDayEmpty = true;
       hotels.forEach((hotel) => {
         const rooms = hotel.rooms;
         // console.log("rooms.length:", rooms.length);
@@ -277,13 +295,40 @@ exports.searchHotels = (request, response, next) => {
         Transaction.find({ hotel: hotel._id })
           .then((trans) => {
             if (trans.length > 0) {
-              // console.log("trans[0]:", trans[0]);
+              // console.log("trans.length:", trans.length);
 
+              // điều kiện phòng trống:
+              // đến ngày 5 phòng đó đã được checkout => phòng trống
               trans.forEach((tran) => {
-                stayCalendar.forEach((date) => {});
+                stayCalendar.forEach((date) => {
+                  // console.log("date:", date);
+                  // console.log("tran.dateEnd:", tran.dateEnd);
+
+                  // const roomEmptyToday = true;
+                  // so sánh date này với dateEnd của tran
+                  // => nếu date > dateEnd => phòng trống
+                  // => nếu date  = dateEnd => phòng trống
+                  // => nếu date < dateEnd => phòng chưa trống
+                  // nếu phòng trống 3 ngày push tất cả item
+                  // của mảng room tran vào mảng result
+                  // compare 2 date to know is later, earlier or same time
+                  const compare = compareDate(date, tran.dateEnd);
+                  // console.log("compare:", compare);
+                  if (compare === "later" || compare === "same") {
+                    allDayEmpty &= true;
+                  } else {
+                    allDayEmpty &= false;
+                  }
+                });
+                if (allDayEmpty) {
+                  // console.log("trans.rooms:", trans.rooms);
+                  tran.rooms.forEach((room) => emptyRoomFromTran.push(room));
+                }
+                allDayEmpty = true;
               });
+              console.log("emptyRoomFromTran:", emptyRoomFromTran);
             } else {
-              //
+              // trường hợp không có tran nào
             }
           })
           .catch((err) => console.log("::ERROR:", err));
